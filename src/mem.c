e6f97c2a9f5909db8c1bcc9a855fc92fbc02b0ac
Devon Gardner <devon@goosur.com>
2025-01-27 00:45:53 -0500

    formatting and type changes

--- src/mem.c
+++ src/mem.c
@@ -3,13 +3,13 @@
 #include "mem.h"
 
 /* Read memory from external process using `process_vm_readv`
- * @param int pid        The target process id
- * @param void *address  The address to read from in target process
- * @param void *buffer   The buffer to store read data into
- * @param ssize_t nbytes The number of bytes to read
+ * @param pid_t   pid     The target process id
+ * @param void *  address The address to read from in target process
+ * @param void *  buffer  The buffer to store read data into
+ * @param ssize_t nbytes  The number of bytes to read
  * @see process_vm_readv()
  */
-void read_mem(int pid, void *address, void *buffer, ssize_t nbytes) {
+void read_mem(pid_t pid, void *address, void *buffer, size_t nbytes) {
   struct iovec local[1];
   struct iovec remote[1];
 
@@ -21,21 +21,19 @@ void read_mem(int pid, void *address, void *buffer, ssize_t nbytes) {
 
   ssize_t nread = process_vm_readv(pid, local, 1, remote, 1, 0);
   if (nread != nbytes) {
-    printf("nread = %d, expected = %d\n", (int)nread, (int)nbytes);
-    printf("errno = %d\n", errno);
-    perror("Failed to read bytes: ");
+    perror("[read_mem] Failed to read bytes: ");
     exit(EXIT_FAILURE);
   }
 }
 
 /* Write memory to external process using `process_vm_writev`
- * @param int pid        The target process id
- * @param void *address  The address to write to in target process
- * @param void *buffer   The buffer to write out
- * @param ssize_t nbytes The number of bytes to write
+ * @param pid_t   pid     The target process id
+ * @param void *  address The address to write to in target process
+ * @param void *  buffer  The buffer to write out
+ * @param ssize_t nbytes  The number of bytes to write
  * @see process_vm_writev()
  */
-void write_mem(int pid, void *address, void *buffer, ssize_t nbytes) {
+void write_mem(pid_t pid, void *address, void *buffer, size_t nbytes) {
   struct iovec local[1];
   struct iovec remote[1];
 
@@ -47,9 +45,7 @@ void write_mem(int pid, void *address, void *buffer, ssize_t nbytes) {
 
   ssize_t nwrote = process_vm_writev(pid, local, 1, remote, 1, 0);
   if (nwrote != nbytes) {
-    printf("nwrote = %d, expected = %d\n", (int)nwrote, (int)nbytes);
-    printf("errno = %d\n", errno);
-    perror("Failed to write bytes: ");
+    perror("[write_mem] Failed to write bytes: ");
     exit(EXIT_FAILURE);
   }
 }
--- src/mem.h
+++ src/mem.h
@@ -1,9 +1,7 @@
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/uio.h>
 
 
-void read_mem(int pid, void *address, void *buffer, ssize_t nbytes);
-void write_mem(int pid, void *address, void *buffer, ssize_t nbytes);
+void read_mem(pid_t pid, void *address, void *buffer, size_t nbytes);
+void write_mem(pid_t pid, void *address, void *buffer, size_t nbytes);
